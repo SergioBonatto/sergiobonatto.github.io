bd3fccf1de34aeeaa00dfe27b28a5d4f32cfc106
Sergio Bonatto <bonatto@tutanota.com>
2024-11-20 01:10:33 -0300

    add: theme vintage

--- JS/footer-component.js
+++ JS/footer-component.js
@@ -10,20 +10,20 @@ class Footer extends HTMLElement {
           <div class="brand">
             <a href="#home" class="logo logo-alt"><span>Bonatto</span></a>
             <p>Bonatto&trade; 2024</p>
-            <p>Todos os direitos reservados.</p>
+            <p>All rights reserved.</p>
           </div>
           <div class="social grid">
             <a href="https://www.instagram.com/Sergio_Bonatto/" target="_blank">
-              <i class="icon-instagram"></i>
+              Instagram
             </a>
             <a href="https://github.com/SergioBonatto/" target="_blank">
-              <img src="assets/fonts/github.svg" alt="GitHub" />
+              Github
             </a>
             <a href="https://twitter.com/fiBonatto" target="_blank">
-              <img src="assets/fonts/twitter.svg" alt="Twitter" />
+              X
             </a>
             <a href="https://www.linkedin.com/in/sergiobonatto/" target="_blank">
-              <img src="assets/fonts/linkedin.svg" alt="LinkedIn" />
+              Linkedin
             </a>
           </div>
         </div>
--- JS/header-component.js
+++ JS/header-component.js
@@ -1,92 +1,95 @@
 // header-component.js
 class HeaderComponent extends HTMLElement {
-    constructor() {
-      super();
-    }
+  constructor() {
+    super();
+  }
 
-    connectedCallback() {
-      this.innerHTML = `
+  connectedCallback() {
+    this.innerHTML = `
         <header id="header">
           <nav class="container">
             <a class="logo" href="#"><span>Bonatto</span></a>
+            <div class="toggle icon-menu">☰</div>
+            <div class="toggle icon-close">✖</div>
             <div class="menu">
               <ul class="grid">
-                <li><a class="title" href="https://sergiobonatto.github.io/#home">Início</a></li>
-                <li><a class="title" href="https://sergiobonatto.github.io/#about">Sobre</a></li>
-                <li><a class="title" href="https://sergiobonatto.github.io/h#contact">Contato</a></li>
+                <li><a class="title" href="https://sergiobonatto.github.io/#home">Home</a></li>
+                <li><a class="title" href="https://sergiobonatto.github.io/#about">About</a></li>
+                <li><a class="title" href="https://sergiobonatto.github.io/#contact">Contact</a></li>
                 <li><a class="title" href="https://sergiobonatto.github.io/blog.html">Blog</a></li>
                 <li><a class="title" href="https://naoehsavio.github.io/learning/docs/Kind/index.html" target="_blank">Kind Book</a></li>
               </ul>
             </div>
-            <div class="toggle icon-menu"></div>
-            <div class="toggle icon-close"></div>
           </nav>
         </header>
       `;
 
-      this.setupEventListeners();
-    }
+    this.setupEventListeners();
+  }
 
-    setupEventListeners() {
-      this.toggleMenu();
-      this.esconderMenuAoClicar();
-      window.addEventListener('scroll', () => {
-        this.changeHeaderWhenScroll();
-        this.activateMenuAtCurrentSection();
+  setupEventListeners() {
+    this.toggleMenu();
+    this.hideMenuOnClick();
+    window.addEventListener('scroll', () => {
+      this.changeHeaderWhenScroll();
+      this.activateMenuAtCurrentSection();
+    });
+  }
+
+  toggleMenu() {
+    const nav = this.querySelector('#header nav');
+    const toggleMenu = this.querySelector('.icon-menu');
+    const toggleClose = this.querySelector('.icon-close');
+
+    toggleMenu.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+
+    toggleClose.addEventListener('click', () => {
+      nav.classList.toggle('show');
+    });
+  }
+
+  hideMenuOnClick() {
+    const links = this.querySelectorAll('nav ul li a');
+
+    for (const link of links) {
+      link.addEventListener('click', () => {
+        this.querySelector('#header nav').classList.remove('show');
       });
-    }
-
-    toggleMenu() {
-      const nav = this.querySelector('#header nav');
-      const toggle = this.querySelectorAll('nav .toggle');
-
-      for (const element of toggle) {
-        element.addEventListener('click', () => {
-          nav.classList.toggle('show');
-        });
-      }
-    }
-
-    esconderMenuAoClicar() {
-      const links = this.querySelectorAll('nav ul li a');
-
-      for (const link of links) {
-        link.addEventListener('click', () => {
-          this.querySelector('#header nav').classList.remove('show');
-        });
-      }
-    }
-
-    changeHeaderWhenScroll() {
-      const header = this.querySelector('#header');
-      const navHeight = header.offsetHeight;
-
-      if (window.scrollY >= navHeight) {
-        header.classList.add('scroll');
-      } else {
-        header.classList.remove('scroll');
-      }
-    }
-
-    activateMenuAtCurrentSection() {
-      const sections = document.querySelectorAll('main section[id]');
-      const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
-
-      for (let section of sections) {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.offsetHeight;
-        const sectionId = section.getAttribute('id');
-
-        const checkpointStart = checkpoint >= sectionTop;
-        const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
-
-        if (checkpointStart && checkpointEnd) {
-          this.querySelector(`nav ul li a[href*=${sectionId}]`)?.classList.add('active');
-        } else {
-          this.querySelector(`nav ul li a[href*=${sectionId}]`)?.classList.remove('active');
-        }
-      }
     }
   }
 
-  customElements.define('header-component', HeaderComponent);
+  changeHeaderWhenScroll() {
+    const header = this.querySelector('#header');
+    const navHeight = header.offsetHeight;
+
+    if (window.scrollY >= navHeight) {
+      header.classList.add('scroll');
+    } else {
+      header.classList.remove('scroll');
+    }
+  }
+
+  activateMenuAtCurrentSection() {
+    const sections = document.querySelectorAll('main section[id]');
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4;
+
+    for (let section of sections) {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const sectionId = section.getAttribute('id');
+
+      const checkpointStart = checkpoint >= sectionTop;
+      const checkpointEnd = checkpoint <= sectionTop + sectionHeight;
+
+      if (checkpointStart && checkpointEnd) {
+        this.querySelector(`nav ul li a[href*=${sectionId}]`)?.classList.add('active');
+      } else {
+        this.querySelector(`nav ul li a[href*=${sectionId}]`)?.classList.remove('active');
+      }
+    }
+  }
+}
+
+customElements.define('header-component', HeaderComponent);
