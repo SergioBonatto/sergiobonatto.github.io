72253a09a0dc46f6160ce18aa9aa1fe6f0ee7834
SergioBonatto <bonatto@tutanota.com>
2024-12-06 00:17:46 -0300

    small changes

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,44 @@
-// Função para mostrar o botão de voltar ao topo
-function backToTop() {
-  const backToTopButton = document.querySelector('.back-to-top');
-
-  if (scrollY >= 360) {
-    backToTopButton.classList.add('show');
-  } else {
-    backToTopButton.classList.remove('show');
+document.addEventListener('DOMContentLoaded', () => {
+  // Função para exibir/ocultar o botão de voltar ao topo
+  function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top');
+    if (window.scrollY >= 560) {
+      backToTopButton.classList.add('show');
+    } else {
+      backToTopButton.classList.remove('show');
+    }
   }
-}
 
+  // Adiciona evento de clique para rolar até a div .visitor-counter
+  document.querySelector('.back-to-top').addEventListener('click', () => {
+    const visitorCounter = document.querySelector('.visitor-counter');
+    if (visitorCounter) {
+      visitorCounter.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      console.warn('Elemento .visitor-counter não encontrado!');
+    }
+  });
 
+  // Inicialização das funções
+  window.addEventListener('scroll', () => {
+    backToTop();
+  });
+});
 
-// Inicialização das funções
+document.addEventListener('DOMContentLoaded', () => {
+  const menuButton = document.querySelector('.icon-menu');
+  const closeButton = document.querySelector('.icon-close');
+  const menu = document.querySelector('.menu');
 
-window.addEventListener('scroll', () => {
-  backToTop();
+  menuButton.addEventListener('click', () => {
+    menu.classList.add('show');
+    menuButton.style.display = 'none';
+    closeButton.style.display = 'block';
+  });
+
+  closeButton.addEventListener('click', () => {
+    menu.classList.remove('show');
+    menuButton.style.display = 'block';
+    closeButton.style.display = 'none';
+  });
 });
