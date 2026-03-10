6dedf0b0646e44720d939f66a3a5c3c03df1c6a5
SergioBonatto <bonatto@tutanota.com>
2026-03-10 17:04:14 -0300

    update: packer

---
+++ tools/packer.h
@@ -0,0 +1,17 @@
+#ifndef TOOLS_PACKER_H
+#define TOOLS_PACKER_H
+
+#define MAX_POSTS	256
+#define MAX_PATH	512
+#define MAX_SLUG	128
+#define MAX_TITLE	256
+#define MAX_DATE	32
+
+struct post {
+	char path[MAX_PATH];
+	char slug[MAX_SLUG];
+	char title[MAX_TITLE];
+	char date[MAX_DATE];
+};
+
+#endif
